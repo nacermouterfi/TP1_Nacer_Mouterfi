bd629fa87749f0c8a83f6756fb34a75322abf159
nacermouterfi <nacermouterfi@hotmail.fr>
2023-06-12 15:14:25 -0400

    Rajout partie formulaire

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+const validateForm = () =>{
+    const form =document.getElementById('form');
+    const firstName =document.getElementById('firstName');
+    const lastName =document.getElementById('lastName');
+    const email =document.getElementById('email');
+    const textArea =document.getElementById('textArea');
+}
